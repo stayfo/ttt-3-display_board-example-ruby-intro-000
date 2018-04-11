205d36c97c54306ac7c4462d2c5a148967a3709b
stayfo@gmail.com <stayfo@gmail.com>
2018-04-11 23:08:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   cell = "  "
-  pipes = "|"
+  pipes = "|   |"
   lines = "-----------"
   puts "#{cell} #{pipes} #{cell} #{pipes} #{cell}"
   puts "#{lines}"
